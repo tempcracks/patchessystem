5d296ae9888512358cde5756fcbefa18fd3b9fec
tempcracks <sernautmoka@gmail.com>
2025-10-28 06:09:42 +0000

    Update patch_st.s

--- patch_st.s
+++ patch_st.s
@@ -67,7 +67,39 @@ logger_create_stdout:
   testq %rax, %rax
   jz  .create_strout_error
 
+  movq %rax,%rbxlogger_init:
+  pushq %rbp
+  movq %rsp, %rbp
+  movl %esi, LOGGER_FD_OFFSET(%rdi)
+  movb %dl, LOGGER_LEVEL_OFFSET(%rdi)
+  movq $0, LOGGER_LEVEL_OFFSET+1(%rdi)
+  popq %rbp
+  retq
+.size logger_init, .-logger_init
+
+#logger creation function
+#logger_t* logger_create_stdout(uint8_t min_level)
+.globl logger_create_stdout
+.type logger_create_stdout, @function
+logger_create_stdout:
+  pushq %rbp
+  movq  %rsp, %rbp
+  pushq %rbx
+
+  #allocate logger structure
+  movq $LOGGER_SIZE, %rdi
+  call malloc
+  testq %rax, %rax
+  jz  .create_strout_error
+
   movq %rax,%rbx
+
+  #initialize with stdout (fd=1)
+  movq %rbx,%rdi
+  movq $1, %rsi  #stdout fd
+  movzbl 16(%rbp), %edx  #min_level from stack
+  call logger_init
+
 
   #initialize with stdout (fd=1)
   movq %rbx,%rdi
@@ -86,4 +118,73 @@ logger_create_stdout:
   popq %rbp
   retq
 .size logger_create_stdout, .-logger_create_stdout
+
+# Level to String Conversion
+# const char* level_to_string(uint8_t level)
+.globl level_to_string
+.type level_to_string, @function
+level_to_string:
+  cmpb $LOG_DEBUG, %dil
+  je .debug_str
+  cmpb $LOG_INFO, %dil
+  je .info_str
+  cmpb $LOG_WARNING, %dil
+  je .warning_str
+  cmpb $LOG_ERROR, %dil
+  je .error_str
+
+  #default
+  leaq level_unknown(%rip), %rax
+  retq
+
+.debug_str:
+  leaq level_debug(%rip), %rax
+  retq
+
+.info_str:
+  leaq level_info(%rip), %rax
+  retq
+
+.warning_str:
+  leaq level_warning(%rip), %rax
+  retq
+
+.error_str:
+  leaq level_error(%rip), %rax
+  retq
+.size level_to_string .-level_to_string
+
+# Timestamp Generation
+# void get_timestamp(char* buffer, size_t buffer_size)
+
+.globl get_timestamp
+.type get_timestamp, @function
+get_timestamp:
+  pushq %rbp
+  movq %rsp, %rbp
+  subq $64, %rsp
+
+  leaq -48(%rbp), %rdi #struct tm buffer
+  xorq %rsi, %rsi #time_t = NULL  (current time)
+  call localtime_r
+
+  #format timestamp
+  movq %rax, %rdi
+  movq 16(%rbp), %rsi
+  movq 24(%rbp), %rdx
+  leaq  timestamp_fmt(%rip), %rcx
+  call strftime
+
+  addq $64, %rsp
+  popq %rbp
+  retq
+.size get_timestamp, .-get_timestamp
+
+
+
+
+
+  
+
+
 
